2ccdd192c030d474f1a129fceee76a35cf1286b8
kuznetsovandrey76 <and.rey.q@yandex.ru>
2016-11-04 17:33:55 +0300

    main command

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("Hello, world");
+// 1.
+var start = "Hello, World";
+var first_str = "<h2>My name is Andrey</h2>";
+var second_str = "<p>I live in Rostov</p>";
+document.write(first_str);
+document.write(second_str + "Yahoo!");
+// 2.
+var name = prompt("What is your name?", "Andrey"); 
+document.write("<p>Hello " + name + "</p>");
+var yes = confirm("Sure?");
+document.write(yes);
+// 3.
+var x = prompt("input X?", 1); 
+var y = prompt("Input Y?", 2); 
+var IntegerX = parseInt(x);
+var IntegerY = parseInt(y);
+var result = IntegerX + IntegerY;
+alert(result);
