e17f3f7e0c18b8f1c0dd5319424ae60a02640b1c
kuznetsovandrey76 <and.rey.q@yandex.ru>
2016-11-24 22:40:27 +0300

    loop

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-// 1.
-var start = "Hello, World";
-var first_str = "<h2>My name is Andrey</h2>";
-var second_str = "<p>I live in Rostov</p>";
-document.write(first_str);
-document.write(second_str + "Yahoo!");
-// 2.
-var name = prompt("What is your name?", "Andrey"); 
-document.write("<p>Hello " + name + "</p>");
-var yes = confirm("Sure?");
-document.write(yes);
-// 3.
-var x = prompt("input X?", 1); 
-var y = prompt("Input Y?", 2); 
-var IntegerX = parseInt(x);
-var IntegerY = parseInt(y);
-var result = IntegerX + IntegerY;
-alert(result);
+// ********* while
+// var i = 1;
+// while ( i < 6 ) {
+// 	console.log(i++);
+// }
+
+
+
+// Возведене числа в степень
+// var num = 12,
+// 	exp = 3,
+// 	result = 1,
+// cnt = 0;
+// while (COUNT < exp) {
+// 	result *= num;
+// 	COUNT++;
+// } 
+// console.log(num + " в степени " + exp + " = " + result);
+
+
+
+// ёлка
+// var tree = "",
+// 	branch = 5,
+// 	symbol = "$",
+// 	cnt = 0;
+// while (cnt < branch) {
+// 	tree += symbol;
+// 	console.log(tree);
+// 	cnt++;
+// }
+
+
+// ******* do while
+var myAge = 26;
+do{
+	var age = prompt("Input your age", "");
+} while (age != myAge);
